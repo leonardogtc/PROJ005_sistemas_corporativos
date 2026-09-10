f3b6dada22c800bfa1c6d72a134e80153828d4ce
Leonardo G.T. da Conceicao <leonardogtc@gmail.com>
2026-09-09 23:03:18 -0300

    Alterando arquivos de configuração e criando STATIC

--- core/urls.py
+++ core/urls.py
@@ -14,9 +14,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# pyrefly: ignore [missing-import]
 from django.contrib import admin
+# pyrefly: ignore [missing-import]
+from django.conf import settings
+# pyrefly: ignore [missing-import]
+from django.conf.urls.static import static
+# pyrefly: ignore [missing-import]
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
